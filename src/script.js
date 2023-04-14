6e12bcfad0f342fe4ff197901da590a91321c415
HelgagleH <olyyyya@ukr.net>
2023-04-14 17:54:05 +0300

    Changes in index, styles, script

--- src/script.js
+++ src/script.js
@@ -1,76 +1,101 @@
-let currentDate = new Date();
-
-let day = currentDate.getDay();
-let month = currentDate.getMonth();
-let date = currentDate.getDate();
-let hour = currentDate.getHours();
-if (hour < 10) {
-    hour = `0${hour}`;
-}
-let minute = currentDate.getMinutes();
-if (minute < 10) {
-    minute = `0${minute}`;
-}
-
-let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
-let months = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12"
-];
-
-let currentDateFull = `${days[day]} / ${date}.${months[month]} ${hour}:${minute}`;
-
-function formatDate(newDate) {
-    return currentDateFull;
+function formatDate(timestamp) {
+    let currentDate = new Date(timestamp);
+    let date = currentDate.getDate();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let months = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12"
+    ];
+    let day = days[currentDate.getDay()];
+    let month = months[currentDate.getMonth()];
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return currentDate = `${day} ${date}.${month} / ${hours}:${minutes}`;
 }
 
-let finalTime = document.querySelector("#current-date");
-finalTime.innerHTML = currentDateFull;
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#current-date");
+    let iconElement = document.querySelector("#icon");
 
-function displayWeatherCondition(response) {
-    document.querySelector("#current-city").innerHTML = response.data.name;
-    document.querySelector("#temperature").innerHTML = `${Math.round(response.data.main.temp)} °C`;
-    document.querySelector("#description").innerHTML = response.data.weather[0].main;
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+    windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) === response.data.weather[0].icon;
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-function searchCity(city) {
+function search(city) {
     let apiKey = "2ff29bed3181c3526c35cc5408037f85";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeatherCondition);
+    axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
     event.preventDefault();
-    let city = document.querySelector("#city-enter").value;
-    searchCity(city);
+    let cityInputElement = document.querySelector("#city-enter");
+    search(cityInputElement.value);
 }
 
-function searchLocation(position) {
-    let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
+function displayFahrenheitTemperature(event) {
     event.preventDefault();
-    navigator.geolocation.getCurrentPosition(searchLocation);
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusValue.classList.remove("active");
+    fahrenheitValue.classList.add("active");
+    let fahrenheitTemparature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemparature);
 }
 
-let searchForm = document.querySelector("#submit-form");
-searchForm.addEventListener("submit", handleSubmit);
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusValue.classList.add("active");
+    fahrenheitValue.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
-let currentLocationButton = document.querySelector("#current-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
+let form = document.querySelector("#submit-form");
+form.addEventListener("submit", handleSubmit);
+
+let celsiusTemperature = null;
+
+let fahrenheitValue = document.querySelector("#fahrenheit");
+fahrenheitValue.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusValue = document.querySelector("#celsius");
+celsiusValue.addEventListener("click", displayCelsiusTemperature);
+
+search("Thessaloniki");
+
+
+
+
+
+
+
+
 
