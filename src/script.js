2efc8c4eeb83fd1007849cdc0fdc503f5ac25282
HelgagleH <olyyyya@ukr.net>
2023-04-21 22:20:37 +0300

    Added new styles

--- src/script.js
+++ src/script.js
@@ -70,8 +70,9 @@ function displayForecast(response) {
             forecastHTML =
                 forecastHTML +
                 `
-        <div class="col-2">
-            <div class="other-days">${formatDay(forecastDay.dt)}</div>
+                <div class="col-2 text-center">
+                
+            <div class="other-days" >${formatDay(forecastDay.dt)}</div>
             <img 
             src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
             alt="Weather img" 
@@ -87,6 +88,7 @@ function displayForecast(response) {
                 ${Math.round(forecastDay.temp.min)} °C
                 </span>
             </div>
+            
         </div>
 `;
         }
