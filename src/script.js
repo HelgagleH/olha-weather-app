c71dd2bfaa10df54721264bcb152b7796d5851e9
HelgagleH <olyyyya@ukr.net>
2023-10-09 19:01:38 +0300

    Changed apiKey

--- src/script.js
+++ src/script.js
@@ -99,7 +99,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-    let apiKey = "62bc298785543e137bc6756e514eb1c3";
+    let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 }
@@ -129,7 +129,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-    let apiKey = "62bc298785543e137bc6756e514eb1c3";
+    let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
